bf8b4cce895b1331a019000f1557cdb0ab9345b3
Hiroki Hattori <seagull.kamome@gmail.com>
2017-09-04 23:10:31 +0900

    Add Bluetooth H4 decoder

---
+++ bluetooth_h4/__init__.py
@@ -0,0 +1,10 @@
+##
+## Copyright (C) 2017 Hattori, Hiroki <seagull.kamome@gmail.com>
+## This file is licensed under BSD3.
+##
+
+'''
+This decoder stacks on top of the 'uart' PD and decodes Bluetooth H4 packet.
+'''
+
+from .pd import Decoder
